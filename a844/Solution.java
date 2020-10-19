e6aba8a5d59543d17980e43116088856adb482e2
cy <863234009@qq.com>
2020-10-19 16:48:35 +0800

    10.19

---
+++ a844/Solution.java
@@ -0,0 +1,55 @@
+package a844;
+
+/**
+ * @author: chenyu
+ * @date: 2020/10/19 15:40
+ */
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            if (i >= 0 && j >= 0) {
+                if (S.charAt(i) != T.charAt(j)) {
+                    return false;
+                }
+            } else {
+                if (i >= 0 || j >= 0) {
+                    return false;
+                }
+            }
+            i--;
+            j--;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        solution.backspaceCompare("a##c","#a#c");
+    }
+}
