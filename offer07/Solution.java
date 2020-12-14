9750dea758ba1ecc9675236b7fe6293bbb3d663c
cy <863234009@qq.com>
2020-12-14 21:23:01 +0800

    12.14

--- offer07/Solution.java
+++ offer07/Solution.java
@@ -14,7 +14,7 @@ class TreeNode {
   }
 public class Solution {
     HashMap<Integer, Integer> hashMap=new HashMap<>();
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
         if(preorder.length==0){
             return null;
         }
@@ -28,14 +28,30 @@ public class Solution {
         if(start>end){
             return null;
         }
-        int val=preorder[start];
+        int val=preorder[index];
         TreeNode node=new TreeNode(val);
-        int i=hashMap.get(preorder[index]);
+        int i=hashMap.get(val);
         node.left=back(preorder,index+1,start,i-1);
         node.right=back(preorder,index+i+1-start,i+1,end);
         return node;
 
     }
 
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for(int i=0;i<inorder.length;i++){
+            hashMap.put(inorder[i],i);
+        }
+        return dfs(preorder,0,0,inorder.length);
+    }
 
+    private TreeNode dfs(int[] preorder,int index,int start,int end){
+        if(start>end){
+            return null;
+        }
+        TreeNode node=new TreeNode(preorder[start]);
+        int i=hashMap.get(preorder[index]);
+        node.left=dfs(preorder,index+1,start,i-1);
+        node.right=dfs(preorder,index+i+1-start,i+1,end);
+        return node;
+    }
 }
