70a89121c024b0ec474bd9728b00b18089cfd650
cy <863234009@qq.com>
2020-12-15 11:35:57 +0800

    12.15

---
+++ a738/Solution.java
@@ -0,0 +1,25 @@
+package a738;
+
+/**
+ * @author: chenyu
+ * @date: 2020/12/15 9:36
+ */
+public class Solution {
+    public int monotoneIncreasingDigits(int N) {
+        int n=111111111;
+        int res=0;
+        for(int i=0;i<9;i++){
+            while(res+n>N){
+                n/=10;
+            }
+            res+=n;
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        solution.monotoneIncreasingDigits(10);
+    }
+}
