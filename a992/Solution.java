98bb4aea533383d4402b10d83598f06abc3b7395
cy <863234009@qq.com>
2021-02-09 10:29:50 +0800

    2.9

---
+++ a992/Solution.java
@@ -0,0 +1,50 @@
+package a992;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @author: chenyu
+ * @date: 2021/2/9 9:52
+ */
+public class Solution {
+
+        public int subarraysWithKDistinct(int[] A, int K) {
+            return atMostKDistinct(A, K) - atMostKDistinct(A, K - 1);
+        }
+
+        /**
+         * @param A
+         * @param K
+         * @return 最多包含 K 个不同整数的子区间的个数
+         */
+        private int atMostKDistinct(int[] A, int K) {
+            int len = A.length;
+            int[] freq = new int[len + 1];
+
+            int left = 0;
+            int right = 0;
+            // [left, right) 里不同整数的个数
+            int count = 0;
+            int res = 0;
+            // [left, right) 包含不同整数的个数小于等于 K
+            while (right < len) {
+                if (freq[A[right]] == 0) {
+                    count++;
+                }
+                freq[A[right]]++;
+                right++;
+
+                while (count > K) {
+                    freq[A[left]]--;
+                    if (freq[A[left]] == 0) {
+                        count--;
+                    }
+                    left++;
+                }
+                // [left, right) 区间的长度就是对结果的贡献
+                res += right - left;
+            }
+            return res;
+        }
+}
