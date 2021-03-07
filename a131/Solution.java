3bb7c8ba05bab4ce0db0b73b79e2ce01aab7c631
cy <863234009@qq.com>
2021-03-07 22:04:42 +0800

    3.7

---
+++ a131/Solution.java
@@ -0,0 +1,38 @@
+package a131;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: chenyu
+ * @date: 2021/3/7 9:22
+ */
+public class Solution {
+    List<List<String>> res=new LinkedList<>();
+    boolean[][] dp;
+    public List<List<String>> partition(String s) {
+        int n=s.length();
+        dp=new boolean[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],true);
+        }
+        back(0,s,new LinkedList<>());
+        return res;
+    }
+
+    private void back(int index,String s,LinkedList<String> list){
+        if(index==s.length()){
+            res.add(new LinkedList<>(list));
+            return;
+        }
+        for(int i=index;i<s.length();i++){
+            if(dp[index][i]){
+                list.add(s.substring(index,i+1));
+                back(index,s,list);
+                list.removeLast();
+            }
+        }
+
+    }
+}
