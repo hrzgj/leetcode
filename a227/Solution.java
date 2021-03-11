e2413c535ba689f34f5f8944a05f458aa460b20f
cy <863234009@qq.com>
2021-03-11 10:14:13 +0800

    3.11

---
+++ a227/Solution.java
@@ -0,0 +1,44 @@
+package a227;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author: chenyu
+ * @date: 2021/3/11 9:02
+ */
+class Solution {
+    public int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    default:
+                        stack.push(stack.pop() / num);
+                }
+                preSign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+}
