2c278feffa13abc42a55ec110ccc6991a104a609
cy <863234009@qq.com>
2021-03-15 22:52:38 +0800

    3.15

---
+++ a54/Solution.java
@@ -0,0 +1,37 @@
+package a54;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: chenyu
+ * @date: 2021/3/15 9:06
+ */
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int n=matrix.length,m=matrix[0].length;
+        List<Integer> list=new LinkedList<>();
+        int left=0,right=m-1,top=0,button=n-1;
+        while(left<=right && top<=button){
+            for(int i=left;i<=right;i++){
+                list.add(matrix[top][i]);
+            }
+            for(int i=top+1;i<=button;i++){
+                list.add(matrix[i][right]);
+            }
+            if(left<right && top<button){
+                for(int i=right-1;i>=left;i--){
+                    list.add(matrix[button][i]);
+                }
+                for(int i=button-1;i>top;i--){
+                    list.add(matrix[i][left]);
+                }
+            }
+            left++;
+            right--;
+            top++;
+            button--;
+        }
+        return list;
+    }
+}
