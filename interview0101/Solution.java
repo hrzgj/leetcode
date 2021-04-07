3a79771efdb4356e9681bcf40bb36f158b2f47e1
cy <863234009@qq.com>
2021-04-07 14:55:31 +0800

    4.7

---
+++ interview0101/Solution.java
@@ -0,0 +1,20 @@
+package interview0101;
+
+import java.util.Arrays;
+
+/**
+ * @author: chenyu
+ * @date: 2021/4/7 14:50
+ */
+public class Solution {
+    public boolean isUnique(String astr) {
+        char[] ch=astr.toCharArray();
+        Arrays.sort(ch);
+        for(int i=0;i<astr.length()-1;i++){
+            if(ch[i]==ch[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
